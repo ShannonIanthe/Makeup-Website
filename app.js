00661e11c94ff5d3834f0e6d96213dac15d9c402
Shannon Mettry <shannonmettry12@gmail.com>
2022-05-30 15:31:44 +0200

    finished black text animation

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+//Fetch data
+const api_url = 'https://makeup-api.herokuapp.com/api/v1/products.json'
+async function getMakeup() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    console.log(data)
+}
+
+getMakeup();
+
+
+//Flipcard
 const cards = document.querySelectorAll('.card');
 
 function flipCard(){

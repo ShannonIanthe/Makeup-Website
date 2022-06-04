5f7e48f70e3b48c86871b2b8c5872c192f9b1ea7
Shannon Mettry <shannonmettry12@gmail.com>
2022-06-04 22:06:01 +0200

    messing with api

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-//Fetch data
-const api_url = 'https://makeup-api.herokuapp.com/api/v1/products.json?product_type=lipstick';
+//Fetch data type: lipstick
+const api_url = 'https://makeup-api.herokuapp.com/api/v1/products.json?product_type=lipstick&product_type=eyeliner&price_greater_than=5';
 async function getMakeup() {
     const response = await fetch(api_url);
     const data = await response.json();
     return data;
 }
 
-//Flipcard
-const cards = document.querySelectorAll('.card');
 
-function flipCard(){
-    this.classList.toggle("flip");
-}
-cards.forEach((card) => card.addEventListener("click", flipCard));
-
+//Fetch Data and place in div and img
 window.addEventListener("load", async function () {
     const makeup = await getMakeup();
     const cardContainer = window.document.querySelector("div#card-container");
@@ -36,10 +30,23 @@ window.addEventListener("load", async function () {
 
         const price = window.document.createElement("p");
         price.classList.add("product-price");
-        price.textContent =  `${element.price_sign} ${element.price}`;
+        price.textContent =  `${element.price_sign} ${element.price}0`;
 
         card.appendChild(price);
+
+        const description = window.document.createElement("p");
+        description.classList.add("product-description");
+        description.textContent = element.description;
+        
+        card.appendChild(description);
 
         cardContainer.appendChild(card);
     });
 });
+
+
+/* write a function -if price is 0.00 place 9.00 as price 
+if price <= 0.0 {
+    console.log(9.00);
+}
+*/

5e5e43d95ff3fee478c4f9190b15508cf5ad9e0b
Shannon Mettry <shannonmettry12@gmail.com>
2022-06-05 19:38:05 +0200

    adding api info to new products section

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 //Fetch data type: lipstick
-const api_url = 'https://makeup-api.herokuapp.com/api/v1/products.json?product_type=lipstick&product_type=eyeliner&price_greater_than=5';
+const api_url = 'https://makeup-api.herokuapp.com/api/v1/products.json?product_type=lipstick&price_greater_than=5';
 async function getMakeup() {
     const response = await fetch(api_url);
     const data = await response.json();
     return data;
 }
 
+const apiUrl = 'https://makeup-api.herokuapp.com/api/v1/products.json?product_type=eyeliner&price_greater_than=5'
+async function getEyeLiner() {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data
+}
 
-//Fetch Data and place in div and img
+
+//Fetch Lipstick Data and place in div and img
 window.addEventListener("load", async function () {
     const makeup = await getMakeup();
     const cardContainer = window.document.querySelector("div#card-container");
@@ -45,8 +52,19 @@ window.addEventListener("load", async function () {
 });
 
 
-/* write a function -if price is 0.00 place 9.00 as price 
-if price <= 0.0 {
-    console.log(9.00);
-}
-*/
+//Fetch Eye Shadow Data and place in div and img
+window.addEventListener("load", async function () {
+    const eyeliner = await getEyeLiner();
+    const eyelinerContainer = window.document.querySelector("div#eyeliner-container");
+    eyeliner.forEach((element) => {
+        const card2 = this.window.document.createElement("div");
+        eyeliner.classList.add("card2");
+
+        const image2 = window.document.createElement("img");
+        image2.classList.add("card2-image2");
+        image2.setAttribute("alt", `https://${element.api_featured_image}`);
+        image2.setAttribute("alt", `https://${element.api_featured_image}`);
+
+        card2.appendChild(image2);
+    })
+})

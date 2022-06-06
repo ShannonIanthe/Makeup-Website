8735da975a6ccf3df0f80f84ee55ddfb38aa23ab
Shannon Mettry <shannonmettry12@gmail.com>
2022-06-06 22:41:43 +0200

    finished footer

---
+++ footer.js
@@ -0,0 +1,59 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `
+    <footer>
+      <div class="footer-menu1">
+        <div class="footer-one">
+        <h5>Multi- Dimensional Makeup</h5>
+          <h5>For every one</h5>
+          <h5>For every where</h5>
+          <h5>For every way</h5>
+        </div>
+      
+    
+      <div class="footer-two">
+        <h6>Shop</h6>
+        <h6>Shop all</h6>
+        <h6>Face</h6>
+        <h6>lip</h6>
+        <h6>eye</h6>
+        <h6>Tools & Merch</h6>
+      </div>
+    
+      <div class="footer-three">
+        <h6>About</h6>
+        <h6>About</h6>
+      </div>
+    
+      <div class="footer-four">
+        <h6>Help</h6>
+        <h6>FAQS</h6>
+        <h6>Accessibility</h6>
+        <h6>Affiliates</h6>
+      </div>
+    </div>
+    
+    <div class="footer-menu2">
+      <div><h6>Instagram</h6></div>
+      <div><h6>Facebook</h6></div>
+      <div><h6>tiktok</h6></div>
+      <div><h6>youtube</h6></div>
+    </div>
+    
+    <div class="footer-menu3">
+      <h7>Privacy Policy</h7>
+      <h7>Terms & Conditions</h7>
+      <h7>Accessibility</h7>
+      <h7>CCPA</h7>
+    </div>
+    
+    <div class="footer-menu4">
+      <h7>Smack</h7>
+      <h7>&emsp; Copyright &copy; 2022</h7>
+    </div>
+    
+    </footer>`
+}
+
+createFooter();
